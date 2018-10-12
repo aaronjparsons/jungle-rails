c2de6609b8b7b43567c45ea567abceb9c9681cff
Aaron Parsons <aaron.parsons@live.ca>
2018-10-12 18:05:58 +0000

    removed password validate from user model because has_secure_password already validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: {scope: :email}
-  validates :password, presence: true
   validates :password_confirmation, presence: true
 end
