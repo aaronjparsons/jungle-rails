d52c1f0830f2ac4f22c527719269857d0f2c980c
Aaron Parsons <aaron.parsons@live.ca>
2018-10-12 04:48:04 +0000

    added validation for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {scope: :email}
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
