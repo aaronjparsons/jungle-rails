13c5adc3212d692b050c61b88515f96396eb7445
Aaron Parsons <aaron.parsons@live.ca>
2018-10-12 18:59:28 +0000

    created reviews model and added some test reviews to the database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,32 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+## REVIEWS
+
+puts "Re-creating Reviews ..."
+
+Review.destroy_all
+
+Review.create!({
+  product_id: 1,
+  user_id: 1,
+  description: "This product is super kewl!",
+  rating: 5,
+})
+
+Review.create!({
+  product_id: 2,
+  user_id: 1,
+  description: "This product is hella lame!",
+  rating: 1,
+})
+
+Review.create!({
+  product_id: 2,
+  user_id: 1,
+  description: "This product is okey doke",
+  rating: 3,
+})
+
 
 puts "DONE!"
