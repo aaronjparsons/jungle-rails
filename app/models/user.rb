046cefa2a0618c3481e973d37cbd23c4dcb1928f
Aaron Parsons <aaron.parsons@live.ca>
2018-10-15 21:52:02 +0000

    added tests for User model, and moved user login authentication to a method on user model, instead of sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,19 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: {scope: :email}
+  validates :email, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @lower_email = email.strip.downcase
+    @user = User.where('lower(email) = ?', @lower_email).first
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      @user = User.new(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(@user).to be_valid
+    end
+
+    it "is not valid without a name " do
+      @user = User.new(name: nil, email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      @user = User.new(name: "Test", email: nil, password: "teSteR", password_confirmation: "teSteR")
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      @user = User.new(name: "Test", email: "test@test.com", password: nil, password_confirmation: "teSteR")
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password confirmation" do
+      @user = User.new(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: nil)
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid if password & password confirmation do not match" do
+      @user = User.new(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "rETsET")
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid if email is not unique (case insensitive)" do
+      @user = User.create(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      @new_user = User.new(name: "User Two", email: "tEst@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(@new_user).to_not be_valid
+    end
+
+    it "is not valid if password is too short (6 chars minimum)" do
+      @user = User.new(name: "Test", email: "test@test.com", password: "teSt", password_confirmation: "teSt")
+      expect(@user).to_not be_valid
+    end
+  end
+
+
+  describe '.authenticate_with_credentials' do
+    it "is valid with valid attributes" do
+      @user = User.create(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(User.authenticate_with_credentials("test@test.com", "teSteR")).to_not be nil
+    end
+
+    it "is not valid without a valid email" do
+      @user = User.create(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(User.authenticate_with_credentials("tester@test.com", "teSteR")).to be nil
+    end
+
+    it "is not valid without a valid password" do
+      @user = User.create(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(User.authenticate_with_credentials("test@test.com", "rETsET")).to be nil
+    end
+
+    it "is valid with extra spaces before and/or after their email" do
+      @user = User.create(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(User.authenticate_with_credentials(" test@test.com ", "teSteR")).to_not be nil
+    end
+
+    it "is valid with with case insensitive email" do
+      @user = User.create(name: "Test", email: "test@test.com", password: "teSteR", password_confirmation: "teSteR")
+      expect(User.authenticate_with_credentials("tEst@teSt.com", "teSteR")).to_not be nil
+    end
+  end
+end
